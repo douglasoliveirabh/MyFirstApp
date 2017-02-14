ad6d20a632cbe4d2fe2023784d849ab02d8188d1
Douglas Everton de Oliveira <douglas.oliveira@squadra.com.br>
2017-02-14 12:03:52 -0200

    Include Bootstrapper call in api project

--- src/MyFirstApp.Api/Startup.cs
+++ src/MyFirstApp.Api/Startup.cs
@@ -30,6 +30,7 @@ namespace MyFirstApp.Api
         {
             // Add framework services.
             services.AddMvc();
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
