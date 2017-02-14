faf7b7d1c8fcded9025719bd9153049b6e1dd10c
Douglas Everton de Oliveira <douglas.oliveira@squadra.com.br>
2017-02-14 11:44:04 -0200

    Create Migrations and Update Database

---
+++ src/MyFirstApp.Infra.Data/Migrations/20170214133334_First Database Version.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MyFirstApp.Infra.Data.Context;
+
+namespace MyFirstApp.Infra.Data.Migrations
+{
+    [DbContext(typeof(MyFirstAppContext))]
+    [Migration("20170214133334_First Database Version")]
+    partial class FirstDatabaseVersion
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.Group", b =>
+                {
+                    b.Property<long>("GroupId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<long?>("PermissionId");
+
+                    b.Property<long?>("UserId");
+
+                    b.HasKey("GroupId");
+
+                    b.HasIndex("PermissionId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("Groups");
+                });
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.Permission", b =>
+                {
+                    b.Property<long>("PermissionId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ConstantName");
+
+                    b.Property<long?>("GroupId");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PermissionId");
+
+                    b.HasIndex("GroupId");
+
+                    b.ToTable("Permissions");
+                });
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.User", b =>
+                {
+                    b.Property<long>("UserId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email");
+
+                    b.Property<long?>("GroupId");
+
+                    b.Property<string>("Password");
+
+                    b.Property<string>("Username");
+
+                    b.HasKey("UserId");
+
+                    b.HasIndex("GroupId");
+
+                    b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.Group", b =>
+                {
+                    b.HasOne("MyFirstApp.Domain.Models.Permission")
+                        .WithMany("Groups")
+                        .HasForeignKey("PermissionId");
+
+                    b.HasOne("MyFirstApp.Domain.Models.User")
+                        .WithMany("Groups")
+                        .HasForeignKey("UserId");
+                });
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.Permission", b =>
+                {
+                    b.HasOne("MyFirstApp.Domain.Models.Group")
+                        .WithMany("Permissions")
+                        .HasForeignKey("GroupId");
+                });
+
+            modelBuilder.Entity("MyFirstApp.Domain.Models.User", b =>
+                {
+                    b.HasOne("MyFirstApp.Domain.Models.Group")
+                        .WithMany("Users")
+                        .HasForeignKey("GroupId");
+                });
+        }
+    }
+}
---
+++ src/MyFirstApp.Infra.Data/Migrations/20170214133334_First Database Version.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyFirstApp.Infra.Data.Migrations
+{
+    public partial class FirstDatabaseVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    PermissionId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ConstantName = table.Column<string>(nullable: true),
+                    GroupId = table.Column<long>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.PermissionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(nullable: true),
+                    GroupId = table.Column<long>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    GroupId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    PermissionId = table.Column<long>(nullable: true),
+                    UserId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.GroupId);
+                    table.ForeignKey(
+                        name: "FK_Groups_Permissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "Permissions",
+                        principalColumn: "PermissionId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Groups_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_PermissionId",
+                table: "Groups",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_UserId",
+                table: "Groups",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_GroupId",
+                table: "Permissions",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_GroupId",
+                table: "Users",
+                column: "GroupId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Permissions_Groups_GroupId",
+                table: "Permissions",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Groups_GroupId",
+                table: "Users",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "GroupId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Permissions_PermissionId",
+                table: "Groups");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Users_UserId",
+                table: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
