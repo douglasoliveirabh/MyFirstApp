bb711dd6f03bf8f4196d05f926b18aea4e34267c
Douglas Everton de Oliveira <douglas.oliveirabh@gmail.com>
2017-03-13 23:00:36 -0300

    Alter some tests

---
+++ test/MyFirstApp.Test.Unit/Repository/GroupRepositoryTest.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using MyFirstApp.Domain.Models;
+using MyFirstApp.Infra.Data.Context;
+using MyFirstApp.Infra.Data.Repository;
+using Xunit;
+using System.Linq;
+
+namespace MyFirstApp.Test.Unit.Repository
+{
+    // see example explanation on xUnit.net website:
+    // https://xunit.github.io/docs/getting-started-dotnet-core.html
+    public class GroupRepositoryTest
+    {
+        private GroupRepository Repository;
+
+        public GroupRepositoryTest()
+        {
+            // In memory doesn't work with migrations
+            // We can't use in memory database with migrations
+            var options = new DbContextOptionsBuilder<MyFirstAppContext>()
+                .UseInMemoryDatabase(databaseName: "MyFirstAppDatabaseTest")
+                .Options;
+
+
+            var context = new MyFirstAppContext(); //new MyFirstAppContext(options);
+            context.Database.Migrate();
+
+            this.Repository = new GroupRepository(context);
+        }
+
+        [Fact]
+        public void Should_Add_Group()
+        {
+
+            this.Repository.Add(new Group("Douglas"));
+            this.Repository.Commit();
+
+            var groups = this.Repository.GetAll().ToList();
+
+            Assert.Equal(groups.Count(), 1);
+
+        }
+
+        /*
+                [Fact]
+                public void PassingTest()
+                {
+                    Assert.Equal(4, Add(2, 2));
+                }
+
+                [Fact]
+                public void FailingTest()
+                {
+                    Assert.Equal(5, Add(2, 2));
+                }
+
+                int Add(int x, int y)
+                {
+                    return x + y;
+                }
+                 */
+    }
+
+}
